c843673095288140e4a643bf0afc9173cfa7d6e3
Marc Corretg <m10m96@hotmail.com>
2018-03-13 16:59:33 +0100

    main

--- Cercle.java
+++ Cercle.java
@@ -42,3 +42,48 @@ setY(float yy)
 	Cercle.y = yy
 }
 }
+
+public class Cercle2{
+private float x;
+private float y;
+
+Cercle2()
+{
+	x = 50
+	y = 60
+}
+
+Cercle2(float xx, float yy)
+{
+x=xx;
+y=yy;
+}
+
+String toString()
+{
+	return Cercle2.x & " " & Cercle2.y
+}
+
+bool equals()
+{
+	return Cercle2.x.equals(Cercle2.y)
+}
+
+String getX()
+{
+	return Cercle2.x
+}
+String getY()
+{
+	return Cercle2.y
+}
+
+setX(float xx)
+{
+	Cercle2.x = xx
+}
+setY(float yy)
+{
+	Cercle2.y = yy
+}
+}
