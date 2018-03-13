e3f13305eaacbc5e3e63484c5d0567c379a9c722
Marc Corretg <m10m96@hotmail.com>
2018-03-13 16:35:46 +0100

    cercle constructors

---
+++ Cercle.java
@@ -0,0 +1,27 @@
+public class Cercle{
+private float x;
+private float y;
+
+Cercle()
+{
+	x = 50
+	y = 60
+}
+
+Cercle(float xx, float yy)
+{
+x=xx;
+y=yy;
+}
+
+String toString()
+{
+	return Cercle.x & " " & Cercle.y
+}
+
+bool equals()
+{
+	return Cercle.x.equals(Cercle.y)
+}
+
+}
